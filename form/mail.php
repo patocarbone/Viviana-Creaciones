57ceed7b37257646cee4633809ef4e664d71a1b7
[Patricio Carbone] <[pato.carbone17@gmail.com]>
2022-05-04 20:35:24 -0300

    primer commit

---
+++ form/mail.php
@@ -0,0 +1,19 @@
+<?php
+
+$destinatario = 'vdvpicazo@gmail.com';
+$subject = "Mensaje enviado desde la pagina WEB";
+$name = $_POST['name'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+$header = "Enviado desde la pagina WEB";
+
+$msg =  " 
+        Nombre: ".$name."\n
+        Email: ".$email."\n
+        Mensaje: ".$message.".";
+
+mail($destinatario,$subject,$msg,$header);
+echo "<h4>¡Mail enviado exitosamente!</h2>";
+echo "<script> setTimeout(\"location.href='../index.html'\",1000) </script>";
+
+?>
